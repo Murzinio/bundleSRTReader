5d6bb93cc0de6da0d2a363a100ae6880d4712119
Murzinio <k.pawelczykbsh@gmail.com>
2018-09-23 14:35:02 +0200

    Reading one file at a time to reduce memory usage.

--- app/Main.hs
+++ app/Main.hs
@@ -10,16 +10,33 @@ import Parser
 import Types
 
 main :: IO()
-main = timeIt run
-
-run :: IO()
-run = do
+main = do
     [mainPath] <- getArgs
-    contents <- readBundles mainPath
+    timeIt $ runOptimized mainPath
+
+run :: FilePath -> IO()
+run path = do
+    contents <- readBundles path
     let parsed = parse $ contents
     let strings = map (map (map (Txt.unpack))) parsed
     let toFile = intercalate "\n" $ sort $ concat $ concat strings
     writeFile "test.txt" toFile
+
+runOptimized :: FilePath -> IO()
+runOptimized path = do
+    paths <- getPaths path
+    runOptimizedImp paths
+
+runOptimizedImp :: Paths -> IO()
+runOptimizedImp [] = return ()
+runOptimizedImp tmp = do
+    print $ stripPath $ head tmp
+    content <- readBundle $ head tmp --TODO will need to adjust to single content
+    let parsed = parse $ [content]
+    let strings = map (map (map (Txt.unpack))) parsed
+    let toFile = intercalate "\n" $ sort $ concat $ concat strings
+    appendFile "testAppend.txt" toFile
+    runOptimizedImp $ tail tmp
 
 headContents :: [Content] -> Content
 headContents (x:xs) = x
--- src/Reader.hs
+++ src/Reader.hs
@@ -1,4 +1,9 @@
-module Reader(readBundles) where
+module Reader(
+    readBundles,
+    readBundle,
+    getPaths,
+    stripPath)
+where --TODO choose which are to be exported
 
 import Data.List
 import qualified Data.Text.IO as TxtIO
@@ -7,19 +12,22 @@ import qualified Data.Text as Txt
 
 import Types
 
+stripPath :: FilePath -> FilePath
+stripPath p = takeWhile (/= '\n') p
+
 srtPathPrefix :: Txt.Text
 srtPathPrefix = Txt.pack "environment"
 
 readBundles :: FilePath -> IO [Content]
 readBundles mainPath = do
     paths <- getPaths mainPath
-    let filtered = map (takeWhile (/= '\n')) paths 
-    contents <- mapM (\x -> readBundle x) filtered
+    contents <- mapM (\x -> readBundle x) paths
     return $ contents
 
 readBundle :: FilePath -> IO Txt.Text
 readBundle path = do
-    content <- TxtIO.readFile path
+    let filtered = stripPath path
+    content <- TxtIO.readFile filtered
     if Txt.isInfixOf (srtPathPrefix) content
         then return content
         else return $ Txt.pack ""
